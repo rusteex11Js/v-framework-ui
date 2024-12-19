3e68e367f1cf98540ce3dba3012c435732e77495
rusteex11Js <vinothjsautomation@gmail.com>
2024-12-19 14:41:51 +0530

    base folder structure

--- src/main/base/DriverBase.java
+++ src/main/base/DriverBase.java
@@ -1,6 +1,6 @@
-package org.example;
+package base;
 
-public class Main {
+public class DriverBase {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
