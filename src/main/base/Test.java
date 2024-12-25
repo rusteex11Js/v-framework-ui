d6b7e6dcb9bf62150b5cc310fafdddf6bd0bbb40
rusteex11Js <vinothjsautomation@gmail.com>
2024-12-25 21:30:29 +0530

    added the driver base

--- src/main/base/Test.java
+++ src/main/base/Test.java
@@ -1,45 +1,34 @@
 package main.base;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
+
+import static main.base.DriverBase.getDriver;
 
 public class Test {
     public static void main(String[] args) {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-//        String name = chromeOptions.getBrowserName();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
-        chromeOptions.setAcceptInsecureCerts(true);
-        chromeOptions.setPageLoadTimeout(Duration.of(5, ChronoUnit.SECONDS));
-        chromeOptions.addArguments("--start-maximized");
 
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-       String str =  driver.getTitle();
+        String str =  getDriver().getTitle();
         System.out.println(str);
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        WebElement textBox = getDriver().findElement(By.name("my-text"));
+        WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
 
         textBox.sendKeys("Selenium");
         submitButton.click();
 
-        WebElement message = driver.findElement(By.id("message"));
+        WebElement message = getDriver().findElement(By.id("message"));
         String str1 = message.getText();
 
         System.out.println(str1);
-        driver.quit();
+        getDriver().quit();
     }
 }
